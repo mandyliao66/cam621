cf3bf3338101be7c43f72ff5f7c432628f10007e
mandyliao66 <51842785+mandyliao66@users.noreply.github.com>
2023-06-23 15:55:50 +0400

    Add files via upload

--- rhombi_markers/src/aruco_detection.cpp
+++ rhombi_markers/src/aruco_detection.cpp
@@ -3,6 +3,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/calib3d.hpp>
 #include <image_transport/image_transport.h>
 #include <geometry_msgs/PoseArray.h>
 #include "aruco.h"
@@ -10,42 +11,41 @@
 using namespace std;
 using namespace cv;
 
-//#define _DICTIONARY aruco::DIC
 #define RATE 30
 
 geometry_msgs::PoseArray pose_publishing;
 aruco::MarkerDetector Mdetector;
 aruco::CameraParameters CamParam;
-//aruco::Dictionary dictionary;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
-  // Image processing callback implementation
   ROS_INFO("Processing image");
 
   cv::Mat image;
-try {
-  // Convert the ROS image message to cv::Mat format
-  image = cv::Mat(msg->height, msg->width, CV_8UC3, const_cast<uint8_t*>(msg->data.data()));
-} catch (const cv::Exception& e) {
-  ROS_ERROR("Failed to convert ROS image message to cv::Mat format: %s", e.what());
-  return;
-}
-
-// Perform ArUco marker detection on the image
-std::vector<aruco::Marker> markers;
-Mdetector.detect(image, markers, CamParam, 0.03, false);
-
-// Visualize the detected ArUco markers
-cv::Mat markerImage = image.clone();
-for (const aruco::Marker& marker : markers) {
-  marker.draw(markerImage, cv::Scalar(0, 0, 255), 2);
-}
-
-// Display the image with ArUco marker overlay
-cv::imshow("ArUco Detection", markerImage);
-cv::waitKey(1);
+  try {
+    // Convert the ROS image message to cv::Mat format
+    image = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8)->image;
+  } catch (const cv::Exception& e) {
+    ROS_ERROR("Failed to convert ROS image message to cv::Mat format: %s", e.what());
+    return;
   }
 
+  // Perform ArUco marker detection on the image
+  std::vector<aruco::Marker> markers;
+  Mdetector.detect(image, markers, CamParam, 0.03, false);
+
+  // Visualize the detected ArUco markers
+  cv::Mat markerImage = image.clone();
+  uint8_t i = 0;
+  for (const aruco::Marker& marker : markers) {
+    std::cout << "Marker " << ++i << ":" << marker.Rvec << " " << marker.Tvec << std::endl;
+    // Calculate pose for each marker
+    marker.draw(markerImage, cv::Scalar(0, 0, 255), 2);
+  }
+
+  // Display the image with ArUco marker overlay
+  cv::imshow("ArUco Detection", markerImage);
+  cv::waitKey(1);
+}
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "aruco_detection");
@@ -54,35 +54,21 @@ int main(int argc, char** argv) {
   image_transport::ImageTransport it(nh);
   ros::Rate loop_rate(RATE);
 
-  // Load camera parameters if available
-  /*
-    string camera_params_file = "/home/surgrob/projects/cam/src/spinnaker_sdk_camera_driver/params/gapter1.yaml";
-    cv::FileStorage fs(camera_params_file, cv::FileStorage::READ);
-    if (!fs.isOpened()) {
-        ROS_ERROR("Failed to open camera parameters file: %s", camera_params_file.c_str());
-        return -1;
-    }
-
-    cv::Mat cameraMatrix, distCoeffs;
-    fs["intrinsic_coeffs"] >> cameraMatrix;
-    fs["distortion_coeffs"] >> distCoeffs;
-
-    fs.release();  // Close the file
-
-  CamParam.CameraMatrix = cameraMatrix;
-  CamParam.Distorsion = distCoeffs;
-  */
-
-  // Configure marker detection parameters
   aruco::MarkerDetector::Params detector_params;
   detector_params.detectEnclosedMarkers(true);
-
-  Mdetector.setDictionary("ARUCO_MIP_36h12");  
+  Mdetector.setDictionary("ARUCO_MIP_36h12");
   Mdetector.setParameters(detector_params);
 
-  // Subscribe to the image topic
-  image_transport::Subscriber sub = it.subscribe("/camera_array/cam0/image_raw", 1, imageCallback);
+  sensor_msgs::CameraInfoConstPtr camera_info = ros::topic::waitForMessage<sensor_msgs::CameraInfo>("/camera_array/cam0/camera_info", nh);
+  if (camera_info != NULL) {
+    sensor_msgs::CameraInfo cam_inf = *camera_info;
+    CamParam.setParams(cv::Mat(3, 3, CV_64F, &cam_inf.K[0]), cv::Mat(1, 5, CV_64F, &cam_inf.D[0]), Size(cam_inf.width, cam_inf.height));
+  } else {
+    ROS_INFO("Camera info file received is empty. Exiting...");
+    return -1;
+  }
 
+  image_transport::Subscriber sub = it.subscribe("/camera_array/cam0/image_raw", 1, imageCallback);
   ros::Publisher PosePub = nh.advertise<geometry_msgs::PoseArray>("/pose_stamped", 1);
 
   ROS_INFO("Starting detection");
